282416be94ed8547a260a8d512ba235ebb70d7d8
ADMIN <ADMIN@RAVITEJA>
2022-06-10 16:42:05 +0530

    changes in pom.xml to run testRunner file

--- Carsome/src/test/java/com/runner/TestRunner.java
+++ Carsome/src/test/java/com/runner/TestRunner.java
@@ -1,47 +1,36 @@
 package com.runner;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.TestNGCucumberRunner;
-import cucumber.api.testng.CucumberFeatureWrapper;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
- 
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
 @CucumberOptions(
         features = "src/test/resources/Features",
-        glue = {"src/test/java/com.stepdef"},
-        tags = {"@carsome"},
-        		plugin = {
+        		 glue = "stepdefinitions",
+        tags = ("@carsome1"),
+        plugin = {
                 "pretty",
-                "html:target/cucumber-reports/cucumber-pretty",
                 "json:target/cucumber-reports/CucumberTestReport.json",
-                "rerun:target/cucumber-reports/rerun.txt"
-        },monochrome=true,strict = false,dryRun = false)
-public class TestRunner {
-    private TestNGCucumberRunner testNGCucumberRunner;
-    
-    @BeforeClass(alwaysRun = true)
-    public void setUpClass() throws Exception {
-        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+                "timeline:target/test-output-thread/"
     }
- 
-    //@Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
-    @Test(dataProvider = "features")
-    public void feature(CucumberFeatureWrapper cucumberFeature) {
-        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+    )  
+public class TestRunner extends AbstractTestNGCucumberTests {
+	@Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
     }
- 
-    @DataProvider
-    public Object[][] features() {
-    	 if(testNGCucumberRunner == null){
-    	        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-    	    }
-    	    return testNGCucumberRunner.provideFeatures();
+
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("================ BEFORE SUITE ================");
     }
- 
-    @AfterClass(alwaysRun = true)
-    public void tearDownClass() throws Exception {
-        testNGCucumberRunner.finish();
-    } 
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("================ AFTER SUITE ================");
+    }
 }
--- Carsome/src/test/resources/Features/carsomeUserstory.feature
+++ Carsome/src/test/resources/Features/carsomeUserstory.feature
@@ -2,13 +2,16 @@
 @carsome
 Feature: Verification of Perodua Axia car in Carsome website
 
-  
+  @carsome1
   Scenario: To verify Perodua Axia is available in Carsome
     Given user is on carsome homepage
     Then click on Buy Car and select Perodua from the dropdown menu
     And Verify Perodua Axia cars available to buy
 
- 
+ @carsome2
   Scenario: To verify the price of available Perodua Axia can be sorted from lowest to highest.
-    Given Recommended dropdown and change it to Lowest Price
+    Given user is on carsome page
+    Then click on Buy Car and select Perodua from the dropdown
+    And check Perodua Axia cars available to buy
+    Then change Recommended dropdown to Lowest Price
     And Verify that the price of the first car is lower than the second car
